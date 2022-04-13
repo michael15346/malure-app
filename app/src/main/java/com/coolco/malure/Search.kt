f16b636ab2628b59848f703109ba397885c6047f
michael15346 <mzadorskyy@gmail.com>
2022-04-13 18:56:02 +0300

    Fix audio recorder whhen the data does not exist

--- app/src/main/java/com/coolco/malure/Search.kt
+++ app/src/main/java/com/coolco/malure/Search.kt
@@ -8,6 +8,7 @@ import android.media.*
 import android.media.AudioFormat.*
 import android.media.AudioRecord.getMinBufferSize
 import android.os.Build
+import android.os.Build.VERSION.SDK_INT
 import android.os.Bundle
 //import androidx.compose.ui.viewinterop.AndroidView
 import android.os.SystemClock.sleep
@@ -67,21 +68,56 @@ class Search : AppCompatActivity() {
                         Manifest.permission.RECORD_AUDIO
                     ) -> {
                         val bufSize = 480000
-                        binding.searchImage.setOnClickListener(){}
-                        val ar = AudioRecord(MediaRecorder.AudioSource.MIC, 48000, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT, bufSize)
+                        binding.searchImage.setOnClickListener(null)
+                        var ar: AudioRecord
+                        if (SDK_INT >= android.os.Build.VERSION_CODES.R){
+                            try{
+
+                                ar = AudioRecord(MediaRecorder.AudioSource.MIC, 48000, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_OPUS, bufSize)
+                            }
+                            catch(e: Exception){ Log.d("RECORDER", "Failed to record in Opus at 48kHz")
+                                try{
+                                    ar = AudioRecord(MediaRecorder.AudioSource.MIC, 48000, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_AAC_LC, bufSize)
+                                }
+                                catch(ex: Exception){
+                                    Log.d("RECORDER", "Failed to record in AAC at 48kHz")
+                                    ar = AudioRecord(MediaRecorder.AudioSource.MIC, 48000, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT, bufSize)
+                                }
+                            }
+                        }
+                        else if (SDK_INT >= android.os.Build.VERSION_CODES.P){
+                            try{
+
+                                ar = AudioRecord(MediaRecorder.AudioSource.MIC, 48000, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_AAC_LC, bufSize)
+                            }
+                            catch(e: Exception){
+                                Log.d("RECORDER", "Failed to record in AAC LC at 48kHz")
+                                ar = AudioRecord(MediaRecorder.AudioSource.MIC, 48000, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT, bufSize)
+                            }
+                        }
+                        else {
+                            ar = AudioRecord(MediaRecorder.AudioSource.MIC, 48000, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT, bufSize)
+                        }
                         binding.searchProgressText.setText(R.string.bird_search_in_progress)
                         model.getSearchResults(ar, bufSize).observe(this, Observer<MutableMap<String, Double>>{birds ->
                             Log.d("MainThread", birds.toString())
                             binding.searchProgressText.text = ""
-                            val intent = Intent(this, Results::class.java)
-                            birds.forEach { (key, value) ->
-                                birdViewModel.insert(Bird(key))
-                                intent.putExtra(
-                                    key,
-                                    value
-                                )
+                            if (!birds.isNullOrEmpty()){
+                                val intent = Intent(this, Results::class.java)
+                                birds.forEach { (key, value) ->
+                                    birdViewModel.insert(Bird(key))
+                                    intent.putExtra(
+                                        key,
+                                        value
+                                    )
+                                }
+                                startActivity(intent)
+
                             }
-                            startActivity(intent)
+                            else
+                            {
+                                binding.searchProgressText.text = "Птицы не найдены!"
+                            }
 
                             binding.searchImage.setOnClickListener(a)
                         })
